184767c1ffa1286a6b95624c846c4d2551c59a4b
takato-miura <takato.miura03@gmail.com>
2025-06-26 22:14:42 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+<body>
+  <p>
+    <?php
+    $associative=['name'=>'onion', 'price'=>200, 'weight'=>160];
+
+    print_r($associative);
+    ?>
+  </p>
+  
+</body>
+</html>
